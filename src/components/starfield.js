a40d68da019b7d0c915724388a8100bdee3dcd7d
mattstyles <matt@veryfizzyjelly.com>
2015-09-30 22:32:00 +0100

    #add star generation values

--- src/components/starfield.js
+++ src/components/starfield.js
@@ -3,17 +3,39 @@ import Pixi from 'pixi.js'
 
 import APP from 'constants/app'
 import { colorToVal } from 'utils/color'
+import { lerp } from 'mathutil'
 
 export default class Starfield {
     constructor() {
         this.container = new Pixi.Container()
         this.tex = null
+
+        // Container density
+        this.density = .0005 * APP.get( 'CANVAS_WIDTH' ) * APP.get( 'CANVAS_HEIGHT' )
+
+        /**
+         * Star values
+         */
+        this.scale = {
+            min: .75,
+            max: 1.25
+        }
+
+        this.alpha = {
+            min: .5,
+            max: 1
+        }
+
+        this.color = {
+            from: [ 0xff, 0xa0, 0xb8 ],
+            to: [ 0xff, 0xd0, 0xe2 ]
+        }
     }
 
     init() {
         this.tex = Pixi.loader.resources[ 'assets/star3x3.png' ].texture
 
-        for( let i = 0; i < APP.get( 'NUM_STARS' ); i++ ) {
+        for( let i = 0; i < this.density; i++ ) {
             this.container.addChild( this.createStar() )
         }
     }
@@ -21,10 +43,10 @@ export default class Starfield {
     createStar() {
         let star = new Pixi.Sprite( this.tex )
         star.position.set( ~~( Math.random() * APP.get( 'CANVAS_WIDTH' ) ), ~~( Math.random() * APP.get( 'CANVAS_HEIGHT' ) ) )
-        let scale = ( Math.random() * .5 ) + .75
+        let scale = lerp( Math.random(), this.scale.min, this.scale.max )
         star.scale.set( scale, scale )
-        star.alpha = .5 + ( Math.random() * .5 )
-        star.tint = colorToVal( [ 0xff, 0xa0, 0xb8 ], [ 0xff, 0xd0, 0xe2 ] )
+        star.alpha = lerp( Math.random(), this.alpha.min, this.alpha.max )
+        star.tint = colorToVal( this.color.from, this.color.to )
 
         return star
     }
